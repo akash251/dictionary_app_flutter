7c0ceeb61ca24e7f7b99a0c6148030ef44a7d7bc
akash251 <akashkamati246@gmail.com>
2022-02-27 09:05:52 +0545

    search button bug fixed

--- lib/UI/HomePage.dart
+++ lib/UI/HomePage.dart
@@ -50,7 +50,7 @@ class HomePage extends StatelessWidget {
                   ),
                   IconButton(
                       onPressed: () {
-                        String value = searchController.toString();
+                        String value = searchController.text.trim();
                         ref.watch(dictionaryProvider.notifier).searchWord(value);
                       },
                       icon: Icon(
